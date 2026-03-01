72ce9618adcaeb2c6c5b8b6f792a5337e801dbfc
paplauskis <paplauskaskostas@gmail.com>
2026-03-01 16:35:56 +0200

    add parent tracking

--- dfs.go
+++ dfs.go
@@ -1,42 +1,54 @@
 package main
 
-import "errors"
+import (
+	"errors"
+)
 
 func DepthFirstSearch(graph Graph) []Node {
-	visited := make([]bool, graph.End.ID*2)
+	approxGraphSize := graph.End.ID * 2
+	visited := make([]bool, approxGraphSize)
 	history := make([]Node, 0)
+	prnts := make([]Node, graph.End.ID*2)
+	for i := range prnts {
+		prnts[i] = Node{ID: NoParent}
+	}
 
 	if len(graph.AdjacencyList[graph.Start.ID]) == 0 {
 		panic(errors.New("starting vertex doesn't have any neighbors"))
 	}
 
-	path, _ := solveDFS(graph, graph.Start, graph.End, visited, history)
+	path, _ := solveDFS(graph, graph.Start, graph.End, visited, history, prnts)
 
-	return path
+	return reconstructPath(path, graph.Start, graph.End)
 }
 
-// todo edit DFS to return solution path instead of history
-func solveDFS(graph Graph, currNode, endNode Node, visited []bool, history []Node) ([]Node, bool) {
+func solveDFS(graph Graph, currNode, endNode Node, visited []bool, history, prnts []Node) ([]Node, bool) {
 	if visited[currNode.ID] {
-		return history, false
+		return prnts, false
 	}
 
 	history = append(history, currNode)
 	visited[currNode.ID] = true
 
 	if currNode == endNode {
-		return history, true
+		return prnts, true
 	}
 
 	for _, node := range graph.AdjacencyList[currNode.ID] {
 		var endNodeFound bool
+
+		if visited[node.ID] {
+			continue
+		}
+		prnts[node.ID] = currNode
+
 		// goes to first neighbor always (depth)
 		// in this case - down -> right -> left -> up (based on directions array)
-		history, endNodeFound = solveDFS(graph, node, endNode, visited, history)
+		history, endNodeFound = solveDFS(graph, node, endNode, visited, history, prnts)
 		if endNodeFound {
-			return history, true
+			return prnts, true
 		}
 	}
 
-	return history, false
+	return prnts, false
 }
