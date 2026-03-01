2e073d19c22a10b208ad29ed7e6a5fb44a084ac0
paplauskis <paplauskaskostas@gmail.com>
2026-03-01 13:08:32 +0200

    edit history array to contain Node structs

--- dfs.go
+++ dfs.go
@@ -2,11 +2,11 @@ package main
 
 import "errors"
 
-func DepthFirstSearch(graph Graph) []int {
-	visited := make([]bool, graph.End*2)
-	history := make([]int, 0)
+func DepthFirstSearch(graph Graph) []Node {
+	visited := make([]bool, graph.End.ID*2)
+	history := make([]Node, 0)
 
-	if len(graph.AdjacencyList[graph.Start]) == 0 {
+	if len(graph.AdjacencyList[graph.Start.ID]) == 0 {
 		panic(errors.New("starting vertex doesn't have any neighbors"))
 	}
 
@@ -15,22 +15,23 @@ func DepthFirstSearch(graph Graph) []int {
 	return path
 }
 
-func solveDFS(graph Graph, currNode, endNode int, visited []bool, history []int) ([]int, bool) {
-	if visited[currNode] {
+func solveDFS(graph Graph, currNode, endNode Node, visited []bool, history []Node) ([]Node, bool) {
+	if visited[currNode.ID] {
 		return history, false
 	}
 
 	history = append(history, currNode)
-	visited[currNode] = true
+	visited[currNode.ID] = true
 
 	if currNode == endNode {
 		return history, true
 	}
 
-	for _, n := range graph.AdjacencyList[currNode] {
+	for _, node := range graph.AdjacencyList[currNode.ID] {
 		var endNodeFound bool
 		// goes to first neighbor always (depth)
-		history, endNodeFound = solveDFS(graph, n, endNode, visited, history)
+		// in this case - down -> right -> left -> up (based on directions array)
+		history, endNodeFound = solveDFS(graph, node, endNode, visited, history)
 		if endNodeFound {
 			return history, true
 		}
