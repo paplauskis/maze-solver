bdc7d05d96cf94a8baaa2457181009c2b29b58b8
paplauskis <paplauskaskostas@gmail.com>
2026-03-01 18:47:56 +0200

    add user input to choose algorithm

--- main.go
+++ main.go
@@ -3,21 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	maze := NewMaze(7)
-	maze.Print()
-
+	maze := NewMaze(19)
 	graph := createGraph(maze.Grid)
 
-	for i := 0; i < len(graph.AdjacencyList); i++ {
-		fmt.Println("Node", i, "→", graph.AdjacencyList[i])
+	var userInput string
+	fmt.Println("Available algorithms: 'DFS', 'BFS'")
+	fmt.Print("Chose maze solving algorithm: ")
+	fmt.Scan(&userInput)
+
+	solutionPath, err := solve("BFS", *graph)
+	if err != nil {
+		panic(err)
 	}
+	maze.CreateCoordPath(solutionPath)
 
 	fmt.Printf("Vertices: %d\n", graph.Vertices)
 	fmt.Printf("Edges: %d\n", graph.Edges)
-	fmt.Printf("Start: %d\n", graph.Start)
-	fmt.Printf("End: %d\n", graph.End)
-
-	path := DepthFirstSearch(*graph)
-
-	fmt.Println(path)
+	fmt.Printf("Start: %d\n", graph.Start.ID)
+	fmt.Printf("End: %d\n", graph.End.ID)
+	//for i := 0; i < len(graph.AdjacencyList); i++ {
+	//	fmt.Println("Node", i, "->", graph.AdjacencyList[i])
+	//}
+	maze.Print()
 }
