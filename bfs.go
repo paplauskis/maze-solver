68a1d3bb65b5026b882d5291a61790e2435d5aa4
paplauskis <paplauskaskostas@gmail.com>
2026-04-15 21:14:36 +0300

    move queue to separate file

--- bfs.go
+++ bfs.go
@@ -4,17 +4,6 @@ import (
 	"errors"
 )
 
-type QNode struct {
-	Node Node
-	Next *QNode
-}
-
-type Queue struct {
-	Size  int
-	Start *QNode
-	Tail  *QNode
-}
-
 func solveBFS(graph Graph, startNode, endNode Node) {
 	queue := NewQueue()
 	graph.Visited[startNode.ID] = true
@@ -42,45 +31,4 @@ func solveBFS(graph Graph, startNode, endNode Node) {
 			}
 		}
 	}
-}
-
-func NewQueue() *Queue {
-	return &Queue{
-		Size:  0,
-		Start: nil,
-		Tail:  nil,
-	}
-}
-
-func (n *Queue) Enqueue(node Node) {
-	temp := &QNode{
-		Node: node,
-		Next: nil,
-	}
-
-	if n.Size == 0 {
-		n.Start = temp
-		n.Tail = temp
-	} else {
-		n.Tail.Next = temp
-		n.Tail = temp
-	}
-
-	n.Size++
-}
-
-func (n *Queue) Dequeue() (Node, error) {
-	if n.Start == nil {
-		panic(errors.New("Queue is empty"))
-	}
-
-	dequeuedNode := n.Start
-	n.Start = n.Start.Next
-	n.Size--
-
-	if n.Start == nil {
-		n.Tail = nil
-	}
-
-	return dequeuedNode.Node, nil
 }
